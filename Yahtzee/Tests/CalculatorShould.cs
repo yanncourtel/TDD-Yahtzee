bafab9654c58a84f7c528c79e6725b096681b2ad
Bastien Vieira <bastien.vieira@cdbdx.biz>
2021-07-12 17:39:10 +0200

    refacto: Calculate method

--- Yahtzee/Tests/CalculatorShould.cs
+++ Yahtzee/Tests/CalculatorShould.cs
@@ -7,51 +7,36 @@ namespace Tests
 {
     public class CalculatorShould
     {
-        [Theory]
-        [InlineData(1, 1, 1, 1, 1, Combination.Ones, 5)]
-        [InlineData(1, 1, 1, 1, 2, Combination.Ones, 4)]
-        [InlineData(1, 1, 1, 1, 2, Combination.Twos, 2)]
-        public void Calculate_Given_Dices_For_Combination_The_Expected_Score(int dice1, int dice2, int dice3, int dice4,
-            int dice5, Combination combination, int expectedScore)
-        {
-            // arrange 
-            var calculator = new Calculator();
-            var dices = new int[] {dice1, dice2, dice3, dice4, dice5};
-
-            // act
-            var result = calculator.Calculate(dices, combination);
-
-            // assert
-            result.Should().Be(expectedScore);
-        }
-
         [Fact]
         public void Calculate_With_Invalid_Combination_Throws_Exception()
         {
             // arrange 
             var calculator = new Calculator();
-            var dices = new int[] {1, 1, 1, 1, 1};
+            var dices = new Dice[] { new Dice(1), new Dice(1), new Dice(1), new Dice(1), new Dice(1) };
+            var roll = new Roll(dices);
 
             // act
             // assert
-            calculator.Invoking(x => x.Calculate(dices, (Combination) 100))
+            calculator.Invoking(x => x.Calculate(roll, (Combination) 100))
                 .Should().Throw<ArgumentOutOfRangeException>();
         }
 
         [Fact]
-        public void Calculate_With_Null_Dices_Throws_Exception()
+        public void Calculate_With_Null_Roll_Throws_Exception()
         {
             // arrange 
             var calculator = new Calculator();
 
             // act
             // assert
-            calculator.Invoking(x => x.Calculate((int[]) null, Combination.Ones))
-                .Should().Throw<InvalidDiceException>("Your dices are invalid");
+            calculator.Invoking(x => x.Calculate(null, Combination.Ones))
+                .Should().Throw<ArgumentNullException>();
         }
 
         [Theory]
         [InlineData(1, 1, 1, 1, 1, Combination.Ones, 5)]
+        [InlineData(1, 1, 1, 1, 2, Combination.Ones, 4)]
+        [InlineData(1, 1, 1, 1, 2, Combination.Twos, 2)]
         public void Calculate_Given_Roll_For_Combination_The_Expected_Score(int dice1, int dice2, int dice3, int dice4,
             int dice5, Combination combination, int expectedScore)
         {
--- Yahtzee/Yahtzee/Calculator.cs
+++ Yahtzee/Yahtzee/Calculator.cs
@@ -5,24 +5,20 @@ namespace Yahtzee
 {
     public class Calculator
     {
-        public int Calculate(int[] dices, Combination combination)
-        {
-            if (dices == null)
-            {
-                throw new InvalidDiceException();
-            }
-            return combination switch
-            {
-                Combination.Ones => dices.Where(x => x == 1).Sum(),
-                Combination.Twos => dices.Where(x => x == 2).Sum(),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-        }
-
         public int Calculate(Roll roll, Combination combination)
         {
+            if (roll == null)
+            {
+                throw new ArgumentNullException();
+            }
             var dicesValues = roll.Dices.Select(x => x.Value).ToArray();
-            return Calculate(dicesValues, combination);
+            
+            return combination switch
+            {
+                Combination.Ones => dicesValues.Where(x => x == 1).Sum(),
+                Combination.Twos => dicesValues.Where(x => x == 2).Sum(),
+                _ => throw new ArgumentOutOfRangeException()
+            };
         }
     }
 }
