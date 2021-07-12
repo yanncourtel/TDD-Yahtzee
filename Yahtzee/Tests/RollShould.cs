03fbd0d82f7de8281e5385ab84d90ca0224da512
David Bourasseau <david.bourasseau@cdiscount.com>
2021-07-12 17:10:26 +0200

    red: invalid roll test

--- Yahtzee/Tests/RollShould.cs
+++ Yahtzee/Tests/RollShould.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentAssertions;
 using Xunit;
+using Yahtzee;
 
 namespace Tests
 {
@@ -10,7 +12,29 @@ namespace Tests
         [Fact]
         public void Have_5_Dices()
         {
-            Assert.True(false);
+            // arrange
+            var dices = new Dice[] {new Dice(1), new Dice(1), new Dice(1), new Dice(1), new Dice(1)};
+            
+            // act
+            var roll = new Roll(dices);
+            
+            // assert
+            roll.Dices.Should().HaveCount(5);
+        }
+        
+        [Fact]
+        public void Not_Have_Less_Than_5_Dices()
+        {
+            // arrange
+            var dices = new Dice[] {new Dice(1), new Dice(1)};
+            
+            // act
+            Action result = ()=> new Roll(dices);
+            
+            // assert
+            result.Should().Throw<InvalidRollException>()
+                .WithMessage("A roll must have 5 dices");
+
         }
     }
 }
